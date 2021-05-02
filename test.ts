e41eaa6877537f3900e0575073fa09d048664340
Toshihide Hara <toshihidehr@gmail.com>
2021-05-02 21:55:24 +0900

    Bump

--- test.ts
+++ test.ts
@@ -9,11 +9,38 @@ class Main {
   hello(name: string) {
     return `Hello ${name}`;
   }
+  hello2() {}
 }
 
-const main = () => {
-  const main = new Main();
+class Main2 {}
 
+const main = () => {
+  const f1 = (s: string) => !!s;
+  const f2 = () => {};
+
+  f1('');
+  f2();
+
+  new Main2();
+
+  if (f1('')) {
+    console.log('');
+  }
+
+  const arr = [1, 2, 3];
+
+  arr.map((ele) => {
+    if (ele) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  const main = new Main();
+  const CONST_VARIABLE_WITH__userId = 1;
+
+  console.log(CONST_VARIABLE_WITH__userId);
   console.log(main.hello(NAME));
 
   console.log(PrettierConfig);
